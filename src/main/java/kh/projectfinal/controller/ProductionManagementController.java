c31b593fe31438a5b51f5b49fe65e835d4baf91f
pk6407 <73266533+pk6407@users.noreply.github.com>
2021-02-25 04:23:32 +0900

    생산관리 출력 표

--- src/main/java/kh/projectfinal/controller/ProductionManagementController.java
+++ src/main/java/kh/projectfinal/controller/ProductionManagementController.java
@@ -2,15 +2,16 @@ package kh.projectfinal.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+@RestController
 public class ProductionManagementController {
 
 	@RequestMapping(value = "/WarehousingSelect", method = { RequestMethod.GET, RequestMethod.POST })
 	public ModelAndView WarehousingSelect() {
 		ModelAndView mv = new ModelAndView();
-		
-		mv.setViewName("Warehousing/WarehousingSelect");
+		mv.setViewName("ProductionManagement/Warehousing/WarehousingSelect");
 		return mv;
 	}
 	
@@ -18,7 +19,7 @@ public class ProductionManagementController {
 	public ModelAndView ReleaseSelect() {
 		ModelAndView mv = new ModelAndView();
 		
-		mv.setViewName("Release/ReleaseSelect");
+		mv.setViewName("ProductionManagement/Release/ReleaseSelect");
 		return mv;
 	}
 	
@@ -26,7 +27,7 @@ public class ProductionManagementController {
 	public ModelAndView StockSelect() {
 		ModelAndView mv = new ModelAndView();
 		
-		mv.setViewName("Stock/StockSelect");
+		mv.setViewName("ProductionManagement/Stock/StockSelect");
 		return mv;
 	}
 	
--- src/main/webapp/WEB-INF/jsp/ProductionManagement/Warehousing/WarehousingSelect.jsp
+++ src/main/webapp/WEB-INF/jsp/ProductionManagement/Warehousing/WarehousingSelect.jsp
@@ -5,8 +5,42 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<link rel='stylesheet' type='text/css' href='css/Production/table.css' />
 </head>
 <body>
-
+<div id="table-scroll" class="table-scroll">
+  <div class="table-wrap">
+    <table class="main-table">
+      <thead>
+        <tr>
+          <th class="fixed-side" scope="col">제품 명(제품 코드)</th>
+          <th scope="col">단위</th>
+          <th scope="col">총 가격</th>
+          <th scope="col">거래처(회사 코드)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th class="fixed-side">Left Column</th>
+          <td>Cell content</td>
+          <td>Cell content longer</td>
+          <td>Cell content</td>  
+        </tr>
+        <tr>
+          <th class="fixed-side">Left Column</th>
+          <td>Cell content</td>
+          <td>Cell content longer</td>
+          <td>Cell content</td>
+        </tr>
+        <tr>
+          <th class="fixed-side">Left Column</th>
+          <td>Cell content</td>
+          <td>Cell content longer</td>
+          <td>Cell content</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
 </body>
 </html>
