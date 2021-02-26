d3fca9d4584457f4c4da22ed2f331d8203676ee1
pk6407 <73266533+pk6407@users.noreply.github.com>
2021-02-26 14:19:16 +0900

    Controller new Code

--- src/main/java/kh/projectfinal/controller/ProductionManagementController.java
+++ src/main/java/kh/projectfinal/controller/ProductionManagementController.java
@@ -15,11 +15,67 @@ public class ProductionManagementController {
 		return mv;
 	}
 	
+	@RequestMapping(value= "/WarehousingInsert", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView WarehousingInsert() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Warehousing/WarehousingInsert");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/WarehousingUpdate", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView WarehousingUpdate() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Warehousing/WarehousingUpdate");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/WarehousingView", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView WarehousingView() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Warehousing/WarehousingView");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/WarehousingDelect", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView WarehousingDelect() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Warehousing/WarehousingDelect");
+		return mv;
+	}
+	
 	@RequestMapping(value = "/ReleaseSelect", method = { RequestMethod.GET, RequestMethod.POST })
 	public ModelAndView ReleaseSelect() {
 		ModelAndView mv = new ModelAndView();
 		
 		mv.setViewName("ProductionManagement/Release/ReleaseSelect");
+		return mv;
+	}
+	
+	@RequestMapping(value= "/ReleaseInsert", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView ReleaseInsert() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Release/ReleaseInsert");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/ReleaseUpdate", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView ReleaseUpdate() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Release/ReleaseUpdate");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/ReleaseView", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView ReleaseView() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Release/ReleaseView");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/ReleaseDelect", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView ReleaseDelect() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Release/ReleaseDelect");
 		return mv;
 	}
 	
@@ -31,4 +87,18 @@ public class ProductionManagementController {
 		return mv;
 	}
 	
+	@RequestMapping(value = "/StockUpdate", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView StockUpdate() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Stock/StockUpdate");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/StockView", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView StockView() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("ProductionManagement/Stock/StockView");
+		return mv;
+	
+	}
 }
